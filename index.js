5266cb00089b428c56f2d0681a3c3092e9662861
OptimusPrime <42608456+IshanSakhuja@users.noreply.github.com>
2022-03-27 15:44:18 +0530

    changes

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-function checkNFT(){
-    if(localStorage.getItem('count') == null){
-        localStorage.setItem('count',0);
+console.log("Inside Index.js")
+if(localStorage.getItem('count') == null){
+    localStorage.setItem('count',0);
+}
+let arr1 = [];
+let count = 1;
+while(localStorage.getItem('NFT'+count) != null){
+    arr1.push(localStorage.getItem('NFT'+count));
+    ++count;
+}
+console.log(arr1.length);
+let arrnew = [];
+for(let i=0;i<arr1.length;i++){
+        arrnew.push(document.getElementsByClassName(arr1[i]));
+}
+console.log(arrnew)
+for(let i=0;i<arrnew.length;i++){
+    let ishan = arrnew[i];
+    for(let j=0;j<ishan.length;j++){
+        ishan[j].innerHTML = "SOLD";
     }
-    console.log("Inside CheckNFT function");
+}
+function checkNFT(){
     let nftId = document.getElementById("nftID").value;
     let name = document.getElementById("fullName").value;
     let mail = document.getElementById("mailAddress").value;
     let message = document.getElementById("message").value;
-    console.log(nftId);
     if(checkmail(mail))
     {
         if(checkNFTID(nftId)){
             let value = localStorage.getItem('count');
-            value = value + 1;
-            localStorage.setItem('count',0);
+            ++value;
             localStorage.setItem('count',value);
             localStorage.setItem('NFT'+value,nftId);
         }else{
